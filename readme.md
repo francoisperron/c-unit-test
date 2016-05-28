84fcacd2d6108ced817326fdeda09707bdeba6a0
Francois Perron <fperron@gmail.com>
2016-05-28 14:35:59 -0400

    with readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+A ways to test c code.
+
+Usage:
+make test
+./test
--- src/main.c
+++ src/main.c
@@ -4,6 +4,6 @@ int add(int a, int b);
 
 int main(void)
 {
-  printf("2 + 3 = %d", add(2,3));
+  printf("2 + 3 = %d\n", add(2,3));
   return 0;
 }
